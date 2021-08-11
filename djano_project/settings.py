cad48b04fe3918343eb88f08d727c3a7471c92bc
monkop <70747511+monkop@users.noreply.github.com>
2021-08-11 15:23:41 +0530

    Update settings.py

--- djano_project/settings.py
+++ djano_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ov96^7p*(jodd)zv66s_hb)ed%pv=vt5c0@h%+7@kyw$yt*0jk'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
